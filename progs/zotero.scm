5e2581e8e70d22d31ee0115b047955e320b0ce68
Karl Hegbloom <karl.hegbloom@gmail.com>
2016-05-06 01:36:00 -0600

    WIP: Protocol almost functioning. Can run by hand in scheme session.

--- progs/zotero.scm
+++ progs/zotero.scm
@@ -24,6 +24,7 @@
 (use-modules (ice-9 format))
 (use-modules (json)) ;; Ported from Guile 2.0 to Guile 1.8 by Karl M. Hegbloom.
 
+(sigaction SIGPIPE (lambda (sig) #t))
 
 ;;;; Protocol between tm_zotero and ZoteroTeXmacsIntegration.js
 ;;
@@ -38,13 +39,6 @@
 ;; Base:
 ;;
 
-(define zotero-transaction-id 0)
-
-(define (get-next-zotero-transaction-id!)
-  (set! zotero-transaction-id (+ 1 zotero-transaction-id))
-  (if (> zotero-transaction-id 16383)
-      (set! zotero-transaction-id 1))
-  zotero-transaction-id)
 
 ;; socket family   style        proto
 ;;        PF_INET  SOCK_STREAM  0
@@ -79,14 +73,14 @@
     (ntohl (u32vector-ref v 0))))
 
 
-(define (write-zotero id cmd)
+(tm-define (zotero-write tid cmd)
   (let ((zp (get-zotero-socket-port!)))
     (catch 'system-error
       (lambda ()
         (let* ((cmdv (list->u8vector (map char->integer
                                           (string->list cmd))))
                (len (u8vector-length cmdv)))
-          (write-network-u32 id zp)
+          (write-network-u32 tid zp)
           (write-network-u32 len zp)
           (uniform-vector-write cmdv zp)
           (force-output zp)))
@@ -95,84 +89,102 @@
         (apply throw args)))))
 
 
-(define (read-zotero)
+(tm-define (zotero-read)
   (let ((zp (get-zotero-socket-port!)))
     (catch 'system-error
       (lambda ()
-        (let* ((id (read-network-u32 zp))
+        (let* ((tid (read-network-u32 zp))
                (len (read-network-u32 zp))
                (cmdv (make-u8vector len 0)))
           (uniform-vector-read! cmdv zp)
-          (list id len (list->string (map integer->char 
-                                          (u8vector->list cmdv))))))
+          (list tid len (list->string (map integer->char 
+                                           (u8vector->list cmdv))))))
       (lambda args
         (write args)
         (apply throw args)))))
 
+(tm-define (zotero-select-then-read)
+  (let ((zp (get-zotero-socket-port!)))
+    (with (r w e) (select (list zp) '() '() 0 500000) ;; 0.5 sec
+      (if (not (null? r))
+          (zotero-read)
+          (list 0 0 "")))))
 
+(define zotero-active? #f)
 
-(define zotero-active? (make-ahash-table))
-
-(define (zotero-add id)
-  (ahash-set! zotero-active? id #t)
+(tm-define (zotero-listen)
+  (set! zotero-active? #t)
   (with wait 1
     (delayed
-      (:while (ahash-ref zotero-active? id))
+      (:while zotero-active?)
       (:pause ((lambda () (inexact->exact (round wait)))))
       (:do (set! wait (min (* 1.01 wait) 2500)))
-      (with (id len cmdstr) (read-zotero)
+      (with (tid len cmdstr) (zotero-select-then-read)
         (when (> 0 len)
-          ;;; now what?
-          ;; (with (msg-id msg-cmd) (string->object msg)
-          ;;   (client-eval (list server msg-id) msg-cmd)
           (with (editCommand args) (json-string->scm cmdstr)
-            (with result (apply (string-append "zotero-" editCommand) (cons id args))
-              (write-zotero id (scm->json-string result));;; !!! HERE !!! NOT
-;;; DONE  Library closed!
-            (ahash-remove! zotero->active? id)
-            (set! wait 1)))))))
+            (cond
+              ((string-equal? editCommand "Document_complete")
+               (write-zotero tid (scm->json-string '()))
+               (set! zotero-active? #f)
+               (set! wait 1))
+              (#t (with result (apply (string-append "zotero-" editCommand) args)
+                    (write-zotero tid (scm->json-string result))
+                    (set! wait 1))))))))))
 
 
 
 ;; Integration commands: TeXmacs -> Zotero, no reply, Zotero connects back with
 ;; Editor commands.
 ;;
-(define-macro (make-zotero-integration-command cmd)
-  `(tm-define (,(string-append "zotero-" cmd))
-     (write-zotero (get-next-zotero-transaction-id!) ,cmd)))
+(tm-define (zotero-addCitation)
+  (zotero-write 0 (scm->json-string "addCitation"))
+  (zotero-listen))
 
-(let loop ((cmds '("addCitation"
-                   "editCitation"
-                   "addBibliography"
-                   "editBibliography"
-                   "refresh"
-                   "removeCodes"
-                   "setDocPrefs")))
-     (cond
-       ((null? cmds) #t)
-       (#t (make-zotero-integration-command (car cmds))
-           (loop (cdr cmds)))))
+(tm-define (zotero-editCitation)
+  (zotero-write 0 (scm->json-string "editCitation"))
+  (zotero-listen))
+
+(tm-define (zotero-addBibliography)
+  (zotero-write 0 (scm->json-string "addBibliography"))
+  (zotero-listen))
+
+(tm-define (zotero-editBibliography)
+  (zotero-write 0 (scm->json-string "editBibliography"))
+  (zotero-listen))
+
+(tm-define (zotero-refresh)
+  (zotero-write 0 (scm->json-string "refresh"))
+  (zotero-listen))
+
+(tm-define (zotero-removeCodes)
+  (zotero-write 0 (scm->json-string "removeCodes"))
+  (zotero-listen))
+
+(tm-define (zotero-setDocPrefs)
+  (zotero-write 0 (scm->json-string "setDocPrefs"))
+  (zotero-listen))
 
 
-(define DIALOG_ICON_STOP 0)
-(define DIALOG_ICON_NOTICE 1)
-(define DIALOG_ICON_CAUTION 2)
 
-(define DIALOG_BUTTONS_OK 0)
-(define DIALOG_BUTTONS_OK_OK_PRESSED 1)
+(define-public DIALOG_ICON_STOP 0)
+(define-public DIALOG_ICON_NOTICE 1)
+(define-public DIALOG_ICON_CAUTION 2)
 
-(define DIALOG_BUTTONS_OK_CANCEL 1)
-(define DIALOG_BUTTONS_OK_CANCEL_CANCEL_PRESSED 0)
-(define DIALOG_BUTTONS_OK_CANCEL_OK_PRESSED 1)
+(define-public DIALOG_BUTTONS_OK 0)
+(define-public DIALOG_BUTTONS_OK_OK_PRESSED 1)
 
-(define DIALOG_BUTTONS_YES_NO 2)
-(define DIALOG_BUTTONS_YES_NO_NO_PRESSED 0)
-(define DIALOG_BUTTONS_YES_NO_YES_PRESSED 1)
+(define-public DIALOG_BUTTONS_OK_CANCEL 1)
+(define-public DIALOG_BUTTONS_OK_CANCEL_CANCEL_PRESSED 0)
+(define-public DIALOG_BUTTONS_OK_CANCEL_OK_PRESSED 1)
 
-(define DIALOG_BUTTONS_YES_NO_CANCEL 3)
-(define DIALOG_BUTTONS_YES_NO_CANCEL_CANCEL_PRESSED 0)
-(define DIALOG_BUTTONS_YES_NO_CANCEL_NO_PRESSED 1)
-(define DIALOG_BUTTONS_YES_NO_CANCEL_YES_PRESSED 2)
+(define-public DIALOG_BUTTONS_YES_NO 2)
+(define-public DIALOG_BUTTONS_YES_NO_NO_PRESSED 0)
+(define-public DIALOG_BUTTONS_YES_NO_YES_PRESSED 1)
+
+(define-public DIALOG_BUTTONS_YES_NO_CANCEL 3)
+(define-public DIALOG_BUTTONS_YES_NO_CANCEL_CANCEL_PRESSED 0)
+(define-public DIALOG_BUTTONS_YES_NO_CANCEL_NO_PRESSED 1)
+(define-public DIALOG_BUTTONS_YES_NO_CANCEL_YES_PRESSED 2)
 
 ;;
 ;; Word Processor commands: Zotero -> TeXmacs
@@ -188,40 +200,65 @@
 ;;
 ;; ["Application_getActiveDocument", [int_protocolVersion]] -> [int_protocolVersion, documentID]
 ;;
-(define zotero-protocolVersion 3)
+(define-public (zotero-getDocId)
+  (car (buffer-path)))
 
-(define (zotero-Application-getActiveDocument id pv)
-  (write-zotero id (format #f "[~s, ~s]" zotero-protocolVersion (zotero-getDocId))))
+(tm-define (zotero-Application_getActiveDocument pv)
+  (display* "zotero-Application_getActiveDocument: " pv "\n")
+  (list pv (zotero-getDocId)))
 
 ;; Shows an alert.
 ;;
 ;; ["Document_displayAlert", [documentID, str_dialogText, int_icon, int_buttons]] -> int_button_pressed
-
+;;
+(tm-define (zotero-Document_displayAlert documentID str_dialogText int_icon int_buttons)
+  ;; stub
+  (display* "zotero-Document_displayAlert: " documentID " " str_dialogText " "
+            int_icon " " int_buttons "\n")
+  0)
 
 
 ;; Brings the document to the foreground. (For OpenOffice, this is a no-op on non-Mac systems.)
 ;;
 ;; ["Document_activate", [documentID]] -> null
-
+;;
+(tm-define (zotero-Document_activate documentID)
+  ;; stub?
+  (display* "zotero-Document_activate: " documentID "\n")
+  '())
 
 
 ;; Indicates whether a field can be inserted at the current cursor position.
 ;;
 ;; ["Document_canInsertField", [documentID]] -> boolean
-
+(tm-define (zotero-Document_canInsertField documentID)
+  ;; stub
+  (display* "zotero-Document_canInsertField: " documentID "\n")
+  #t)
 
 
 ;; Stores a document-specific persistent data string. This data
 ;; contains the style ID and other user preferences.
 ;;
 ;; ["Document_setDocumentData", [documentID, str_dataString]] -> null
+;;
+(define zotero-document-data (make-ahash-table)) ;;; todo: store it in aux
 
+(tm-define (zotero-Document_setDocumentData documentID str_dataString)
+  ;; stub
+  (display* "zotero-Document_setDocumentData: " documentID " " str_dataString "\n")
+  (ahash-set! zotero-document-data documentID str_dataString)
+  '())
 
 
 ;; Retrieves data string set by setDocumentData.
 ;;
 ;; ["Document_getDocumentData", [documentID]] -> str_dataString
-
+;;
+(tm-define (zotero-Document_getDocumentData documentID)
+  ;; stub: Todo: get from document aux
+  (display* "zotero-Document_getDocumentData: " documentID "\n")
+  (ahash-ref zotero-document-data documentID ""))
 
 
 ;; Indicates whether the cursor is in a given field. If it is, returns
@@ -234,7 +271,11 @@
 ;;                 resides, or 0 if the field is not in a footnote.
 ;;
 ;; ["Document_cursorInField", [documentID, str_fieldType]] -> null || [fieldID, fieldCode, int_noteIndex]
-
+;;
+(tm-define (zotero-Document_cursorInField documentID str_fieldType)
+  ;; stub
+  (display* "zotero-Document_cursorInField: " documentID " " str_fieldType "\n")
+  '())
 
 
 (define NOTE_IN_TEXT 0)
@@ -247,7 +288,11 @@
 ;; int_noteType, NOTE_IN_TEXT, NOTE_FOOTNOTE, or NOTE_ENDNOTE
 ;;
 ;; ["Document_insertField", [documentID, str_fieldType, int_noteType]] -> [fieldID, fieldCode, int_noteIndex]
-
+;;
+(tm-define (zotero-Document_insertField documentID str_fieldType int_noteType)
+  ;; stub
+  (display* "zotero-Document_insertField: " documentID " " str_fieldType " " int_noteType "\n")
+  (list 0 "ReferenceMark" 0))
 
 
 ;; Get all fields present in the document, in document order.
@@ -255,22 +300,42 @@
 ;; str_fieldType is the type of field used by the document, either ReferenceMark or Bookmark
 ;;
 ;; ["Document_getFields", [documentID, str_fieldType]] -> [[fieldID, ...], [fieldCode, ...], [noteIndex, ...]]
-
+;;
+(tm-define (zotero-Document_getFields documentID str_fieldType)
+  ;; stub
+  (display* "zotero-Document_getFields: " documentID " " str_fieldType "\n")
+  (list (list 0) (list "ReferenceMark") (list 0)))
 
 
 ;; ["Document_convert" ??? (TODO in documentation.)
-
+;;
+(tm-define (zotero-Document_convert . args)
+  ;; stub
+  (display* "zotero-Document_convert: " args "\n")
+  '())
 
 
 ;; ["Document_setBibliographyStyle", ??? (TODO)
-
+;;
+(tm-define (zotero-Document_setBibliographyStyle . args)
+  ;; stub
+  (display* "zotero-Document_setBibliographyStyle: " args "\n")
+  '())
 
 
 ;; Indicates that the given documentID will no longer be used and
 ;; associated resources may be freed.
 ;;
 ;; ["Document_complete", [documentID]] -> null
-
+;;
+;; See: zotero-listen, where this is checked for inline... but also enable it
+;; here since I might need to use it during development, at least.
+;;
+(tm-define (zotero-Document_complete documentID)
+  ;; stub
+  (display* "zotero-Document_complete: " documentID "\n")
+  (set! zotero-active? #f)
+  '())
 
 
 ;; Deletes a field from the document (both its code and its contents).
@@ -279,39 +344,71 @@
 ;; Document_insertField, or Document_getFields.
 ;;
 ;; ["Field_delete", [documentID, fieldID]] -> null
-
+;;
+(tm-define (zotero-Field_delete documentID fieldID)
+  ;; stub
+  (display* "zotero-Field_delete: " documentID " " fieldID "\n")
+  '())
 
 
 ;; Moves the current cursor position to encompass a field.
 ;;
 ;; ["Field_select", [documentID, fieldID]] -> null
-
+;;
+(tm-define (zotero-Field_select documentID fieldID)
+  ;; stub
+  (display* "zotero-Field_select: " documentID " " fieldID "\n")
+  '())
 
 
 ;; Removes the field code from a field, leaving it as plain text.
 ;;
 ;; ["Field_removeCode", [documentID, fieldID]] -> null
-
+;;
+(tm-define (zotero-Field_removeCode documentID fieldID)
+  ;; stub
+  (display* "zotero-Field_removeCode: " documentID " " fieldID "\n")
+  '())
 
 
 ;; Sets the (visible) text of a field.
 ;;
 ;; ["Field_setText", [documentID, fieldID, str_text, isRich]] -> null
-
+;;
+(tm-define (zotero-Field_setText documentID fieldID str_text isRich)
+  ;; stub
+  (display* "zotero-Field_setText: " documentID " " fieldID " " str_text " "
+            isRich "\n")
+  '())
 
 
 ;; Gets the (visible) text of a field.
 ;;
 ;; ["Field_getText", [documentID, fieldID]] -> str_text
-
+;;
+(tm-define (zotero-Field_getText documentID fieldID)
+  ;; stub
+  (display* "zotero-Field_getText: " documentID " " fieldID "\n")
+  "STUB Field Text STUB")
 
 
 ;; Sets the (hidden, persistent) code of a field.
 ;;
 ;; ["Field_setCode", [documentID, fieldID, str_code]] -> null
-
+;;
+(tm-define (zotero-Field_setCode documentID fieldID str_code)
+  ;; stub
+  (display* "zotero-Field_setCode: " documentID " " fieldID " " str_code "\n")
+  '())
 
 
 ;; Converts a field from one type to another.
 ;;
-;; ["Field_convert", [documentID, fieldID, str_fieldType, int_noteType]] -> null
+;; ["Field_convert", [documentID, fieldID, str_fieldType, int_noteType]] ->
+;; null
+;;
+(tm-define (zotero-Field_convert documentID fieldID str_fieldType int_noteType)
+  ;; stub
+  (display* "zotero-Field_convert: " documentID " " fieldID " " str_fieldType
+            " " int_noteType "\n")
+  '())
